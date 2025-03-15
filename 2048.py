e382a03e068ea09ae2fa1c4fd8cf6920331ffe5b
JackDevilGod <151947211+JackDevilGod@users.noreply.github.com>
2025-03-15 18:19:16 +0100

    added main file for the game

---
+++ 2048.py
@@ -0,0 +1,14 @@
+class Game:
+    def __init__(self, size: int):
+        self.size = size
+
+        self.matrix = [[0 for _ in range(self.size)]
+                       for _ in range(self.size)]
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
